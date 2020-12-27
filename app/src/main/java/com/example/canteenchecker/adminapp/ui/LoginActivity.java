caa4868eb3f0ae058afd7e1434e010a87ce4d4b0
Wagner <dominik.waagna@gmail.com>
2020-12-27 17:01:14 +0100

    add login ui

---
+++ app/src/main/java/com/example/canteenchecker/adminapp/ui/LoginActivity.java
@@ -0,0 +1,16 @@
+package com.example.canteenchecker.adminapp.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.canteenchecker.adminapp.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_login);
+  }
+}
