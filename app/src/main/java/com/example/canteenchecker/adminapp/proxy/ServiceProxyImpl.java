729960b392ff75f7f13273b9bc2c36af3c8d4246
Wagner <dominik.waagna@gmail.com>
2020-12-29 09:07:57 +0100

    reindent ServiceProxyImpl

--- app/src/main/java/com/example/canteenchecker/adminapp/proxy/ServiceProxyImpl.java
+++ app/src/main/java/com/example/canteenchecker/adminapp/proxy/ServiceProxyImpl.java
@@ -21,116 +21,116 @@ import retrofit2.http.Query;
 // proxy implementation for API version 1.0
 // https://moc5.projekte.fh-hagenberg.at/CanteenChecker/swagger/index.html
 class ServiceProxyImpl implements ServiceProxy {
-    private static final String SERVICE_BASE_URL = "https://moc5.projekte.fh-hagenberg.at/CanteenChecker/api/";
-    private static final long ARTIFICIAL_DELAY = 100;
+  private static final String SERVICE_BASE_URL = "https://moc5.projekte.fh-hagenberg.at/CanteenChecker/api/";
+  private static final long ARTIFICIAL_DELAY = 0;
 
-    private final Proxy proxy = new Retrofit.Builder()
-            .baseUrl(SERVICE_BASE_URL)
-            .addConverterFactory(ScalarsConverterFactory.create())
-            .addConverterFactory(GsonConverterFactory.create())
-            .build()
-            .create(Proxy.class);
+  private final Proxy proxy = new Retrofit.Builder()
+          .baseUrl(SERVICE_BASE_URL)
+          .addConverterFactory(ScalarsConverterFactory.create())
+          .addConverterFactory(GsonConverterFactory.create())
+          .build()
+          .create(Proxy.class);
 
-    private void causeDelay() {
-        try {
-            Thread.sleep(ARTIFICIAL_DELAY);
-        } catch (InterruptedException ignored) {
-        }
+  private void causeDelay() {
+    try {
+      Thread.sleep(ARTIFICIAL_DELAY);
+    } catch (InterruptedException ignored) {
     }
+  }
 
-    @Override
-    public Collection<Canteen> getCanteens(String filter) throws IOException {
-        causeDelay(); // for testing only
-        Collection<Proxy_CanteenData> canteens = proxy.getCanteens(filter).execute().body();
-        if (canteens == null) {
-            return null;
-        }
-        Collection<Canteen> result = new ArrayList<>(canteens.size());
-        for (Proxy_CanteenData canteen : canteens) {
-            result.add(canteen.toCanteen());
-        }
-        return result;
+  @Override
+  public Collection<Canteen> getCanteens(String filter) throws IOException {
+    causeDelay(); // for testing only
+    Collection<Proxy_CanteenData> canteens = proxy.getCanteens(filter).execute().body();
+    if (canteens == null) {
+      return null;
     }
-
-    @Override
-    public CanteenDetails getCanteen(String canteenId) throws IOException {
-        causeDelay(); // for testing only
-        Proxy_CanteenDetails canteen = proxy.getCanteen(canteenId).execute().body();
-        return canteen != null ? canteen.toCanteenDetails() : null;
+    Collection<Canteen> result = new ArrayList<>(canteens.size());
+    for (Proxy_CanteenData canteen : canteens) {
+      result.add(canteen.toCanteen());
     }
+    return result;
+  }
 
-    @Override
-    public ReviewData getReviewsDataForCanteen(String canteenId) throws IOException {
-        causeDelay(); // for testing only
-        Proxy_CanteenReviewStatistics reviewData = proxy.getReviewStatisticsForCanteen(canteenId).execute().body();
-        return reviewData != null ? reviewData.toReviewData() : null;
+  @Override
+  public CanteenDetails getCanteen(String canteenId) throws IOException {
+    causeDelay(); // for testing only
+    Proxy_CanteenDetails canteen = proxy.getCanteen(canteenId).execute().body();
+    return canteen != null ? canteen.toCanteenDetails() : null;
+  }
+
+  @Override
+  public ReviewData getReviewsDataForCanteen(String canteenId) throws IOException {
+    causeDelay(); // for testing only
+    Proxy_CanteenReviewStatistics reviewData = proxy.getReviewStatisticsForCanteen(canteenId).execute().body();
+    return reviewData != null ? reviewData.toReviewData() : null;
+  }
+
+  @Override
+  public String authenticate(String userName, String password) throws IOException {
+    causeDelay(); // for testing only
+    return proxy.postAuthenticate(userName, password).execute().body();
+  }
+
+  @Override
+  public void createReview(String authToken, String canteenId, int rating, String remark) throws IOException {
+    causeDelay(); // for testing only
+    // TODO make bearer token nicer
+    proxy.postCanteenReview(String.format("Bearer %s", authToken), canteenId, rating, remark).execute();
+  }
+
+  private interface Proxy {
+    @POST("authenticate")
+    Call<String> postAuthenticate(@Query("userName") String userName, @Query("password") String password);
+
+    @GET("canteens")
+    Call<Collection<Proxy_CanteenData>> getCanteens(@Query("name") String name);
+
+    @GET("canteens/{canteenId}")
+    Call<Proxy_CanteenDetails> getCanteen(@Path("canteenId") String canteenId);
+
+    @GET("canteens/{canteenId}/review-statistics")
+    Call<Proxy_CanteenReviewStatistics> getReviewStatisticsForCanteen(@Path("canteenId") String canteenId);
+
+    @POST("canteens/{canteenId}/reviews")
+    Call<Void> postCanteenReview(@Header("Authorization") String authenticationToken, @Path("canteenId") String canteenId, @Query("rating") int rating, @Query("remark") String remark);
+  }
+
+  private static class Proxy_CanteenData {
+    String id;
+    String name;
+    String dish;
+    float dishPrice;
+    float averageRating;
+
+    Canteen toCanteen() {
+      return new Canteen(id, name, dish, dishPrice, averageRating);
     }
+  }
 
-    @Override
-    public String authenticate(String userName, String password) throws IOException {
-        causeDelay(); // for testing only
-        return proxy.postAuthenticate(userName, password).execute().body();
+  private static class Proxy_CanteenDetails {
+    String name;
+    String address;
+    String phoneNumber;
+    String website;
+    String dish;
+    float dishPrice;
+    int waitingTime;
+
+    CanteenDetails toCanteenDetails() {
+      return new CanteenDetails(name, phoneNumber, website, dish, dishPrice, address, waitingTime);
     }
+  }
 
-    @Override
-    public void createReview(String authToken, String canteenId, int rating, String remark) throws IOException {
-        causeDelay(); // for testing only
-        // TODO make bearer token nicer
-        proxy.postCanteenReview(String.format("Bearer %s", authToken), canteenId, rating, remark).execute();
+  private static class Proxy_CanteenReviewStatistics {
+    int countOneStar;
+    int countTwoStars;
+    int countThreeStars;
+    int countFourStars;
+    int countFiveStars;
+
+    ReviewData toReviewData() {
+      return new ReviewData(countOneStar, countTwoStars, countThreeStars, countFourStars, countFiveStars);
     }
-
-    private interface Proxy {
-        @POST("authenticate")
-        Call<String> postAuthenticate(@Query("userName") String userName, @Query("password") String password);
-
-        @GET("canteens")
-        Call<Collection<Proxy_CanteenData>> getCanteens(@Query("name") String name);
-
-        @GET("canteens/{canteenId}")
-        Call<Proxy_CanteenDetails> getCanteen(@Path("canteenId") String canteenId);
-
-        @GET("canteens/{canteenId}/review-statistics")
-        Call<Proxy_CanteenReviewStatistics> getReviewStatisticsForCanteen(@Path("canteenId") String canteenId);
-
-        @POST("canteens/{canteenId}/reviews")
-        Call<Void> postCanteenReview(@Header("Authorization") String authenticationToken, @Path("canteenId") String canteenId, @Query("rating") int rating, @Query("remark") String remark);
-    }
-
-    private static class Proxy_CanteenData {
-        String id;
-        String name;
-        String dish;
-        float dishPrice;
-        float averageRating;
-
-        Canteen toCanteen() {
-            return new Canteen(id, name, dish, dishPrice, averageRating);
-        }
-    }
-
-    private static class Proxy_CanteenDetails {
-        String name;
-        String address;
-        String phoneNumber;
-        String website;
-        String dish;
-        float dishPrice;
-        int waitingTime;
-
-        CanteenDetails toCanteenDetails() {
-            return new CanteenDetails(name, phoneNumber, website, dish, dishPrice, address, waitingTime);
-        }
-    }
-
-    private static class Proxy_CanteenReviewStatistics {
-        int countOneStar;
-        int countTwoStars;
-        int countThreeStars;
-        int countFourStars;
-        int countFiveStars;
-
-        ReviewData toReviewData() {
-            return new ReviewData(countOneStar, countTwoStars, countThreeStars, countFourStars, countFiveStars);
-        }
-    }
+  }
 }
